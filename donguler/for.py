d982ecad8e2ba86a09071a87d3ff368fe9184a58
alperentvs <alperentavas45@gmail.com>
2022-02-02 13:25:06 +0300

    add for.py

---
+++ donguler/for.py
@@ -0,0 +1,35 @@
+sayilar = [1,2,3,6,8,9,15,20]
+
+
+isimler = ['ali','deniz','yagmur']
+# for i in isimler:
+#     print(i)
+
+sayilar = [1,2,3,6,8,9,15,20]
+# for i in sayilar:
+#     print('Merhaba')
+
+isimler = ['ali','deniz','yagmur']
+# for isim in isimler:
+#     print(isim)
+
+isim = "Alperen Tavas"
+# for a in isim:
+#     print(a)
+
+_tuple = [(1,2),(4,5),(6,7)]
+# for a,b in _tuple:
+#     print(a,b)
+
+_dict = {'k1':1,'k2':2,'k3':3}
+for x in _dict:
+    print(x)
+
+for x in _dict:
+    print(_dict,x)
+
+for x in _dict.values():
+    print(x)
+
+for key,value in _dict.items():
+    print(key,value)
